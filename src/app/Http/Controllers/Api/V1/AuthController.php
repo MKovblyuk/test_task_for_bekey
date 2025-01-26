88e558573d7035b8abbfdfd5c739eefab8231616
MKovblyuk <mkvblyuk@gmail.com>
2025-01-26 16:55:13 +0200

    added documentation

--- src/app/Http/Controllers/Api/V1/AuthController.php
+++ src/app/Http/Controllers/Api/V1/AuthController.php
@@ -8,6 +8,11 @@ use App\Http\Requests\Auth\RegisterUserRequest;
 use App\Services\Auth\AuthServiceInterface;
 use Illuminate\Http\Request;
 
+/**
+ * @group Auth management
+ *
+ * APIs for managing authentication
+ */
 class AuthController extends Controller
 {
     public function __construct(
@@ -15,6 +20,30 @@ class AuthController extends Controller
     )
     {}
 
+    /**
+     * Login
+     * 
+     * @unauthenticated
+     * 
+     * @bodyParam password string required string Example: 12QWERTY_3a
+     * 
+     * @response 401 {"message": "Incorrect credentials"}
+     * 
+     * @response 200 {
+     *      "message": "User successfully logged in",
+     *      "token": "1|YGwScKg7jPnZlviC5tGjfSKYy5sFsEsy5odBftn16977e462",
+     *      "userId": 1
+     * }
+     * 
+     * @response 422 {
+     *     "message": "Validation Failed",
+     *      "errors": {
+     *          "email": [
+     *              "The email field is required."
+     *          ]
+     *      }
+     * }
+     */
     public function login(LoginUserRequest $request)
     {
         $result = $this->authService->login($request->validated());
@@ -30,6 +59,34 @@ class AuthController extends Controller
         return response()->json(['message' => 'Incorrect credentials'], 401);
     }
 
+    /**
+     * Register
+     * 
+     * @unauthenticated
+     * 
+     * @bodyParam password string required Example: 12QWERTY_3a
+     * @bodyParam password_confirmation string required Example: 12QWERTY_3a
+     * 
+     * @response 500 {"message": "Registration failed"} scenario="Server error"
+     * 
+     * @response 200 {
+     *      "message": "User successfully registered",
+     *      "token": "1|YGwScKg7jPnZlviC5tGjfSKYy5sFsEsy5odBftn16977e462",
+     *      "userId": 1
+     * }
+     * 
+     * @response 422 {
+     *     "message": "Validation Failed",
+     *      "errors": {
+     *          "name": [
+     *              "The name field is required."
+     *          ],
+     *          "email": [
+     *              "The email field is required."
+     *          ]
+     *      }
+     * }
+     */
     public function register(RegisterUserRequest $request)
     {
         $result = $this->authService->register($request->validated());
@@ -45,6 +102,11 @@ class AuthController extends Controller
         return response()->json(['message' => 'Registration failed'], 500);
     }
 
+    /**
+     * Logout
+     * 
+     * @response 200 {"message": "User logged out"}
+     */
     public function logout(Request $request)
     {
         $this->authService->logout($request);
--- src/app/Http/Controllers/Api/V1/TaskController.php
+++ src/app/Http/Controllers/Api/V1/TaskController.php
@@ -16,6 +16,11 @@ use App\Http\Resources\Tasks\TaskResource;
 use App\Models\Task;
 use Illuminate\Routing\Controllers\HasMiddleware;
 
+/**
+ * @group Task management
+ *
+ * APIs for managing tasks
+ */
 class TaskController extends Controller implements HasMiddleware
 {
     public static function middleware(): array
@@ -23,11 +28,38 @@ class TaskController extends Controller implements HasMiddleware
         return ['auth:sanctum'];
     }
 
+    /**
+     * Get a list of tasks.
+     * 
+     * @queryParam filter[status] string Filter tasks by status. Example: Completed
+     * @queryParam filter[creator_id] string Filter tasks by creator ids. Example: 1,2,5
+     * 
+     * @apiResourceCollection App\Http\Resources\Tasks\TaskResource
+     * @apiResourceModel App\Models\Task
+     * 
+     * @response 401 {"message": "Unauthenticated."}
+     */
     public function index(GetTasksAction $action)
     {
         return new TaskCollection($action->execute());
     }
 
+    /**
+     * Create task.
+     * 
+     * @response 201 {"message": "Task successfully created."}
+     * @response 401 {"message": "Unauthenticated."}
+     * @response 500 {"message": "Task not created"} scenario="Server error"
+     * 
+     * @response 422 {
+     *     "message": "Validation Failed",
+     *      "errors": {
+     *          "name": [
+     *              "The name field is required."
+     *          ]
+     *      }
+     * }
+     */
     public function store(StoreTaskRequest $request, StoreTaskAction $action)
     {
         return $action->execute($request->validated())
@@ -35,11 +67,37 @@ class TaskController extends Controller implements HasMiddleware
             : response()->json(['message' => 'Task not created'], 500);
     }
 
+    /**
+     * Get a single task.
+     * 
+     * @apiResource App\Http\Resources\Tasks\TaskResource
+     * @apiResourceModel App\Models\Task
+     * 
+     * @response 401 {"message": "Unauthenticated."}
+     * @response 404 {"message": "Not Found Resource"}
+     */
     public function show(Task $task)
     {
         return new TaskResource($task);
     }
 
+    /**
+     * Update task.
+     * 
+     * @response 200 {"message": "Task successfully updated"}
+     * @response 401 {"message": "Unauthenticated."}
+     * @response 404 {"message": "Not Found Resource"}
+     * @response 500 {"message": "Task not updated"} scenario="Server error"
+     * 
+     * @response 422 {
+     *     "message": "Validation Failed",
+     *      "errors": {
+     *          "name": [
+     *              "The name field is required."
+     *          ]
+     *      }
+     * }
+     */
     public function update(UpdateTaskRequest $request, Task $task, UpdateTaskAction $action)
     {
         return $action->execute($task, $request->validated())
@@ -47,6 +105,14 @@ class TaskController extends Controller implements HasMiddleware
             : response()->json(['message' => 'Task not updated'], 500);
     }
 
+    /**
+     * Delete task.
+     * 
+     * @response 204 scenario=Success
+     * @response 401 {"message": "Unauthenticated."}
+     * @response 404 {"message": "Not Found Resource"}
+     * @response 400 {"message": "Task not deleted"}
+     */
     public function destroy(Task $task, DeleteTaskAction $action)
     {
         return $action->execute($task)
@@ -54,6 +120,14 @@ class TaskController extends Controller implements HasMiddleware
             : response()->json(['message' => 'Task not deleted'], 400);
     }
 
+    /**
+     * Set the task status as completed.
+     * 
+     * @response 200 {"message": "Task completed"}
+     * @response 401 {"message": "Unauthenticated."}
+     * @response 404 {"message": "Not Found Resource"}
+     * @response 400 {"message": "Task not completed"}
+     */
     public function complete(Task $task, CompleteTaskAction $action)
     {
         return $action->execute($task)
@@ -61,6 +135,14 @@ class TaskController extends Controller implements HasMiddleware
             : response()->json(['message' => 'Task not completed'], 400);
     }
 
+    /**
+     * Set the task status as uncompleted.
+     * 
+     * @response 200 {"message": "Task uncompleted"}
+     * @response 401 {"message": "Unauthenticated."}
+     * @response 404 {"message": "Not Found Resource"}
+     * @response 400 {"message": "Task not uncompleted"}
+     */
     public function uncomplete(Task $task, UncompleteTaskAction $action)
     {
         return $action->execute($task)
